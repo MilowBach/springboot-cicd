8d431fe10f9acaddee8f2ec194b1d572f5d218c1
Mouloud <mbachtarzi@altea-si.com>
2021-11-21 20:46:26 -0500

    Test

--- src/test/java/com/dev/cicd/data/controller/DataControllerTest.java
+++ src/test/java/com/dev/cicd/data/controller/DataControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class DataControllerTest {
+class DataControllerTest {
 
     @Autowired
     DataController dataController;
