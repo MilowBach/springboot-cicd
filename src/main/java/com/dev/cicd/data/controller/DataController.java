6faac0506c42d5f7e1e6c15af49bcf7e89c596c0
Mouloud <mbachtarzi@altea-si.com>
2021-12-13 11:35:08 -0500

    Test

--- src/main/java/com/dev/cicd/data/controller/DataController.java
+++ src/main/java/com/dev/cicd/data/controller/DataController.java
@@ -13,7 +13,7 @@ public class DataController {
 
     @GetMapping("/")
     public String healthCheck(){
-        return "HEALTH CHECK OK YES!";
+        return "HEALTH CHECK OK YES WOW!";
     }
 
     @GetMapping("/version")
